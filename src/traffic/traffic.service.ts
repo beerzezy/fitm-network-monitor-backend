9fbc2e52559d936ff09ed28124b3f34f210a5426
phureerak-so <phureerak.so@tnis.com>
2020-07-09 17:11:52 +0700

    add calculate days

--- src/traffic/traffic.service.ts
+++ src/traffic/traffic.service.ts
@@ -116,34 +116,34 @@ export class TrafficService {
   async getTrafficDataByDays(deviceName: string): Promise<TrafficInterface[]> {   
     const data = []
     const dateNow = new Date(Date.now());
-    
     const dateTime = moment().format('YYYY-MM-DD')
+    
+    var testDate = new Date(moment(dateNow,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+    console.log("testDate : ", testDate)
+    // let year = dateNow.getFullYear()
+    // // let month = dateNow.getMonth()+1  //because january start 0
+    // let monthPrevoius = dateNow.getMonth()
+    // let monthCurrent = monthPrevoius+1
+    // let monthStr = ''
+    // if (monthCurrent < 10) {
+    //   monthStr = '0' + monthCurrent
+    // } else {
+    //   monthStr = monthCurrent.toString()
+    // }
 
-    let year = dateNow.getFullYear()
-
-    // let month = dateNow.getMonth()+1  //because january start 0
-    let monthPrevoius = dateNow.getMonth()
-    let monthCurrent = monthPrevoius+1
-    let monthStr = ''
-    if (monthCurrent < 10) {
-      monthStr = '0' + monthCurrent
-    } else {
-      monthStr = monthCurrent.toString()
-    }
-
-    let d = dateNow.getDate()
-    let dayPreviousStr = ''
-    if (d - 1 < 0) {
-      let lastDayOfMonth = new Date(year, monthPrevoius, 0).getDate()
-      dayPreviousStr = lastDayOfMonth.toString()
-    }else {
-      if (d - 1 < 10){
-        dayPreviousStr = '0' + (d - 1).toString()
-      } else {
-        dayPreviousStr = (d - 1).toString()
-      }
-    }
-    console.log("dStr : ", dayPreviousStr)
+    // let d = dateNow.getDate()
+    // let dayPreviousStr = ''
+    // if (d - 1 < 0) {
+    //   let lastDayOfMonth = new Date(year, monthPrevoius, 0).getDate()
+    //   dayPreviousStr = lastDayOfMonth.toString()
+    // }else {
+    //   if (d - 1 < 10){
+    //     dayPreviousStr = '0' + (d - 1).toString()
+    //   } else {
+    //     dayPreviousStr = (d - 1).toString()
+    //   }
+    // }
+    // console.log("dStr : ", dayPreviousStr)
 
     let h = dateNow.getHours()
     let hStr = ''
@@ -161,7 +161,7 @@ export class TrafficService {
       mStr = m.toString()
     }
 
-    const stTime = `${year}-${monthStr}-${dayPreviousStr} ${hStr}:${mStr}`
+    const stTime = `${dateTime} 00:00`
     const stFormat = moment(stTime).format('x')
     const stFormatx = stFormat.substr(0, 10)
     const start = new Date(moment.unix(parseInt(stFormatx)).add(7, 'hour').toString())
@@ -190,9 +190,67 @@ export class TrafficService {
       if (start < timeCompare){
         if (timeCompare < end)
         data.push({ id: result.id, timestamp: time, ...other })
-      }
-      
+      } 
     })
+
+    // var testDate = moment('2020-07-10T08:05:44.936Z','YYYY-MM-DD HH:mm:ss.SSS').format('YYYY-MM-DDTHH:mm:ss.SSSZ')
+    // var testDateNow = moment(dateNow,'YYYY-MM-DD HH:mm:ss.SSS').format('YYYY-MM-DDTHH:mm:ss.SSSZ')
+
+    // if (lastTime < startTime) {
+    //   console.log("test if testDate",lastTime)
+    //   console.log("test if testDateNow",startTime)
+    // } else {
+    //   console.log("test else testDate",lastTime)
+    //   console.log("test else testDateNow",startTime)
+    // }
+
+    // var testDate = moment(`${dateTime}T08:00:00.000Z`,'YYYY-MM-DD HH:mm:ss.SSS').format('YYYY-MM-DDTHH:mm:ss.SSSZ')
+    // console.log("test testDate Change",testDate)
+
+    var inHour = []
+    var endHour = end.getHours()
+    var startHour = endHour-1
+    var inboundSum = 0 , outboundSum = 0
+    var i = 0
+ 
+    var lastTime = new Date(moment(`${dateTime}T${endHour}:00:00.000Z`,'YYYY-MM-DD HH:mm:ss.SSS').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+    var startTime = new Date(moment(`${dateTime}T${startHour}:00:00.000Z`,'YYYY-MM-DD HH:mm:ss.SSS').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+    console.log("lastTime",lastTime)
+    console.log("startHour",startTime)
+
+    var isChangeHour = false
+    
+    data.forEach(result => {
+      let { timestamp, outbound, inbound } = result
+      console.log("timestamp",timestamp)
+
+      if (isChangeHour) {
+        endHour = startHour
+        startHour = endHour-1
+        lastTime = new Date(moment(`${dateTime}T${endHour}:00:00.000Z`,'YYYY-MM-DD HH:mm:ss.SSS').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+        startTime = new Date(moment(`${dateTime}T${startHour}:00:00.000Z`,'YYYY-MM-DD HH:mm:ss.SSS').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+      }
+
+      if (lastTime > timestamp && startTime < timestamp){
+        isChangeHour = false
+        inboundSum = inboundSum + inbound
+        outboundSum = outboundSum + outbound
+        i++
+      } else {
+        inboundSum = inboundSum / i
+        outboundSum = outboundSum / i
+        inHour.push({ id: result.id, timestamp: timestamp, inbound: inboundSum , outbound: outboundSum })
+        isChangeHour = true
+        endHour = startHour
+        inboundSum = 0
+        outboundSum = 0
+        i = 0
+      }
+    })
+    inHour.forEach(dd => {
+      console.log("inHour",dd)
+    })
+
 
     return data
   }
