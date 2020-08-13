31525034e73b791852f9d4ba5feb9444e34a1e1a
phureerak-so <phureerak.so@tnis.com>
2020-08-14 00:19:02 +0700

    update query data solutions

--- src/traffic/traffic.service.ts
+++ src/traffic/traffic.service.ts
@@ -24,7 +24,8 @@ export class TrafficService {
     var end = new Date(moment(`${dateTime} ${h}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
 
     const results = await this.networkRef.doc(deviceName).collection('traffic')
-      .orderBy('timestamp', 'desc')
+      .where("epoch", ">=", +start).where("epoch", "<", +end)
+      .orderBy('epoch', 'desc')
       .get()
       
     if (results.empty) {
@@ -33,11 +34,7 @@ export class TrafficService {
     results.forEach(result => {
       const { timestamp, ...other } = result.data()
       const time = moment.unix(timestamp._seconds).format('HH:mm  DD-MM-YYYY')
-      const timeCompare = new Date(moment.unix(timestamp._seconds).add(7, 'hour').toString())
-      if (start < timeCompare){
-        if (timeCompare < end)
         data.push({ id: result.id, timestamp: time, ...other })
-      }
     })
     return data
   }
@@ -52,8 +49,10 @@ export class TrafficService {
     console.log("start pic",start)
     console.log("end pic",end)
     const results = await this.networkRef.doc(deviceName).collection('traffic')
-      .orderBy('timestamp', 'desc')
+      .where("epoch", ">=", +start).where("epoch", "<", +end)
+      .orderBy('epoch', 'desc')
       .get()
+
       
     if (results.empty) {
       return
@@ -140,18 +139,19 @@ export class TrafficService {
     const data = []
     const dateNow = new Date(Date.now());
     const dateTime = moment().format('YYYY-MM-DD')
+
     var h = dateNow.getHours()
     var m = dateNow.getMinutes()
-    var hPre = h - 1
 
-    var start = new Date(moment(`${dateTime} ${hPre}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
-    console.log("startl : ", start)
+    var start = new Date(moment(`${dateTime} ${h}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(6, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+    var end = new Date(moment(`${dateTime} ${h}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
 
-    const end = new Date(moment(`${dateTime} ${h}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
-    console.log("end : ", end)
+    console.log("start hour : ", start)
+    console.log("end hour : ", end)
 
     const results = await this.networkRef.doc(deviceName).collection('traffic')
-      .orderBy('timestamp', 'desc')
+      .where("epoch", ">=", +start).where("epoch", "<", +end)
+      .orderBy('epoch', 'desc')
       .get()
      
     if (results.empty) {
@@ -161,13 +161,7 @@ export class TrafficService {
     results.forEach(result => {
       const { timestamp, ...other } = result.data()
       const time = moment.unix(timestamp._seconds).format('HH:mm  DD-MM-YYYY')
-      const timeCompare = new Date(moment.unix(timestamp._seconds).add(7, 'hour').toString())
-
-      if (start < timeCompare){
-        if (timeCompare < end)
         data.push({ id: result.id, timestamp: time, ...other })
-      }
-      
     })
 
     return data
@@ -182,16 +176,56 @@ export class TrafficService {
     let m = dateNow.getMinutes()
 
     var start = new Date(moment(`${dateTime} 00:00`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
-    console.log("start day : ", start)
+    var end = new Date(moment(`${dateTime} ${h}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
 
-    const end = new Date(moment(`${dateTime} ${h}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+    console.log("start day : ", start)
     console.log("end day : ", end)
 
+    // var epochStart = +start
+    // var epochEnd = +end
+    // var resetEpoch = 0 , multiEpoch = 0
+    
+    // do {
+    //   var eEnd = epochStart + 300000
+    //   let results = await this.networkRef.doc(deviceName).collection('traffic')
+    //     .where("epoch", ">=", epochStart).where("epoch", "<", eEnd)
+    //     .get()
 
+    //   if (results.empty) {
+    //     epochStart = epochStart + 2400000
+    //     resetEpoch++
+    //     console.log("epochStart in if : ", epochStart)
+    //     console.log("resetEpoch in if : ", resetEpoch)
+    //   } else {
+    //     results.forEach(result => {
+    //       const { timestamp, ...other } = result.data()
+    //       const time = moment.unix(timestamp._seconds).format('HH:mm  DD-MM-YYYY')
+    //         data.push({ id: result.id, timestamp: time, ...other })
+    //     })
+      
+    //     multiEpoch = resetEpoch * 2400000
+    //     epochStart = epochStart - multiEpoch + 3600000
+    //     resetEpoch = 0
+    //     console.log("epochStart in else : ", epochStart)
+    //     console.log("multiEpoch in else : ", multiEpoch)
+    //   }
+
+    // } while(epochStart < epochEnd)
+
+    // if (data.length == 0) {
+    //   return
+    // }
+
+    // data.sort((a, b) => (a.epoch < b.epoch) ? 1 : -1)
+    // data.forEach(result => {
+    //   console.log("data : ", result)
+    // })
+    
     const results = await this.networkRef.doc(deviceName).collection('traffic')
-      .orderBy('timestamp', 'desc')
+      .where("epoch", ">=", +start).where("epoch", "<", +end)
+      .orderBy('epoch', 'desc')
       .get()
-     
+    
     if (results.empty) {
       return
     }
@@ -199,24 +233,19 @@ export class TrafficService {
     results.forEach(result => {
       const { timestamp, ...other } = result.data()
       const time = moment.unix(timestamp._seconds).format('HH:mm  DD-MM-YYYY')
-      const timeCompare = new Date(moment.unix(timestamp._seconds).add(7, 'hour').toString())
-      
-      if (start < timeCompare){
-        if (timeCompare < end)
         data.push({ id: result.id, timestamp: time, ...other })
-      } 
     })
 
     var inHour = []
-    var endHour = h+1
+    var endHour = h + 1
     var startHour = endHour-1
     var inboundSum = 0 , outboundSum = 0
     var inboundMean = 0 ,outboundSumMean = 0
     var i = 0
 
-    var lastTime = new Date(moment(`${dateTime} ${endHour}:00:00.000`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+    //var lastTime = new Date(moment(`${dateTime} ${endHour}:00:00.000`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
     var startTime = new Date(moment(`${dateTime} ${startHour}:00:00.000`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
-    var hourTimeShow = moment(lastTime,'YYYY-MM-DD HH:mm:ss.SSS').subtract(7, 'hour').format('HH:mm DD-MM-YYYY')
+    var hourTimeShow = moment(startTime,'YYYY-MM-DD HH:mm:ss.SSS').subtract(7, 'hour').format('HH:mm')
     var isChangeHour = false
     
     data.forEach(result => {
@@ -225,7 +254,7 @@ export class TrafficService {
       if (isChangeHour) {
         endHour = startHour
         startHour = endHour-1
-        lastTime = new Date(moment(`${dateTime} ${endHour}:00:00.000Z`,'YYYY-MM-DD HH:mm:ss').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ').toString())
+        //lastTime = new Date(moment(`${dateTime} ${endHour}:00:00.000Z`,'YYYY-MM-DD HH:mm:ss').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ').toString())
         startTime = new Date(moment(`${dateTime} ${startHour}:00:00.000Z`,'YYYY-MM-DD HH:mm:ss').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ').toString())
       }
 
@@ -237,8 +266,11 @@ export class TrafficService {
       } else {
         inboundMean = inboundSum / i
         outboundSumMean = outboundSum / i
-        hourTimeShow = moment(lastTime,'YYYY-MM-DD HH:mm:ss.SSS').subtract(7, 'hour').format('HH:mm DD-MM-YYYY')
-        inHour.push({ id: result.id, timestamp: hourTimeShow, inbound: inboundMean , outbound: outboundSumMean })
+        hourTimeShow = moment(startTime,'YYYY-MM-DD HH:mm:ss.SSS').subtract(7, 'hour').format('HH:mm')
+        if (inboundMean != null) {
+          console.log("inboundMean",inboundMean)
+          inHour.push({ id: result.id, timestamp: hourTimeShow, inbound: inboundMean , outbound: outboundSumMean })
+        }
         isChangeHour = true
         endHour = startHour
         inboundSum = 0
@@ -246,35 +278,36 @@ export class TrafficService {
         i = 0
       }
     })
-    if (inboundSum != 0) {
+
+    if (inboundSum != 0 && inboundMean != null) {
       inboundMean = inboundSum / i
       outboundSumMean = outboundSum / i
-      hourTimeShow = moment(lastTime,'YYYY-MM-DD HH:mm:ss.SSS').subtract(7, 'hour').format('HH:mm DD-MM-YYYY')
+      hourTimeShow = moment(startTime,'YYYY-MM-DD HH:mm:ss.SSS').subtract(7, 'hour').format('HH:mm')
       inHour.push({ id: data[data.length - 1].id, timestamp: hourTimeShow, inbound: inboundMean , outbound: outboundSumMean })
     }
-
+ 
     return inHour
   }
 
   async getTrafficDataByMonth(deviceName: string): Promise<TrafficInterface[]> {   
     const data = []
     const dateNow = new Date(Date.now());
-    
     const dateTime = moment().format('YYYY-MM-DD')
 
     var year = dateNow.getFullYear()
     var month = dateNow.getMonth()+1
-
-    var start = new Date(moment(`${year}-${month}-01 00:00`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
-
     let h = dateNow.getHours()
     let m = dateNow.getMinutes()
 
-    const end = new Date(moment(`${dateTime} ${h}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+    var start = new Date(moment(`${year}-${month}-01 00:00`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+    var end = new Date(moment(`${dateTime} ${h}:${m}`,'YYYY-MM-DD HH:mm:ss.SSS').add(7, 'hour').format('YYYY-MM-DDTHH:mm:ss.SSSZ'))
+
     console.log("end : ", end)
 
     const results = await this.networkRef.doc(deviceName).collection('traffic')
-      .orderBy('timestamp', 'desc')
+      .where("epoch", ">=", +start).where("epoch", "<", +end)
+      .orderBy('epoch', 'desc')
+      .limit(1)
       .get()
      
     if (results.empty) {
@@ -284,14 +317,48 @@ export class TrafficService {
     results.forEach(result => {
       const { timestamp, ...other } = result.data()
       const time = moment.unix(timestamp._seconds).format('HH:mm  DD-MM-YYYY')
-      const timeCompare = new Date(moment.unix(timestamp._seconds).add(7, 'hour').toString())
-      
-      if (start < timeCompare){
-        if (timeCompare < end)
         data.push({ id: result.id, timestamp: time, ...other })
-      }
-      
     })
+
+    // var epochStart = +start
+    // var epochEnd = +end
+    // var resetEpoch = 0 , multiEpoch = 0
+    
+    // do {
+    //   var eEnd = epochStart + 300000
+    //   let results = await this.networkRef.doc(deviceName).collection('traffic')
+    //     .where("epoch", ">=", epochStart).where("epoch", "<", eEnd)
+    //     .get()
+
+    //   if (results.empty) {
+    //     epochStart = epochStart + 3600000
+    //     resetEpoch++
+    //     console.log("epochStart in if : ", epochStart)
+    //     console.log("resetEpoch in if : ", resetEpoch)
+    //   } else {
+    //     results.forEach(result => {
+    //       const { timestamp, ...other } = result.data()
+    //       const time = moment.unix(timestamp._seconds).format('HH:mm  DD-MM-YYYY')
+    //         data.push({ id: result.id, timestamp: time, ...other })
+    //     })
+      
+    //     multiEpoch = resetEpoch * 3600000
+    //     epochStart = epochStart - multiEpoch + 86400000 + 100000
+    //     resetEpoch = 0
+    //     console.log("epochStart in else : ", epochStart)
+    //     console.log("multiEpoch in else : ", multiEpoch)
+    //   }
+
+    // } while(epochStart < epochEnd)
+
+    // if (data.length == 0) {
+    //   return
+    // }
+
+    // data.sort((a, b) => (a.epoch < b.epoch) ? 1 : -1)
+    // data.forEach(result => {
+    //   console.log("data : ", result)
+    // })
 
     var day = dateNow.getDate()
 
@@ -345,7 +412,9 @@ export class TrafficService {
       dayTimeShow = moment(startTime,'YYYY-MM-DD HH:mm:ss.SSS').subtract(7, 'hour').format('DD-MM-YYYY')
       inDay.push({ id: data[data.length - 1].id, timestamp: dayTimeShow, inbound: inboundMean , outbound: outboundSumMean })
     }
-
+    inDay.forEach(resutl => {
+      console.log(resutl)
+    })
     return inDay
   }
 
