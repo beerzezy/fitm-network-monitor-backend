22f67481e413f164689435ce1ab9eb6d046f87fe
unknown <phanudetkawilai2016@gmail.com>
2020-11-08 15:52:11 +0700

    change DB

--- src/database/firebase.config.ts
+++ src/database/firebase.config.ts
@@ -1,8 +1,8 @@
 import * as admin from 'firebase-admin'
-const serviceAccount = require('../database/serviceAccount.json')
+const serviceAccount = require('src/database/serviceAccount.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://fitm-monitordev-1c7a0.firebaseio.com'
+  databaseURL: 'https://fitm-monitordev-2ccdc.firebaseio.com'
 })
 export const db = admin.firestore()
